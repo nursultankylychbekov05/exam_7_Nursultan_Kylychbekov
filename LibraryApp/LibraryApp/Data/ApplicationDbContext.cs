515e8198b1764a64f54f447a906b1b52f6dd6fbf
Kylychbekov Nursultan <nursultankylychbekov05@gmail.com>
2026-09-05 15:48:51 +0600

    Добавлена модель Book и настроено подключение к PostgreSQL

---
+++ LibraryApp/LibraryApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using LibraryApp.Models;
+
+namespace LibraryApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ LibraryApp/LibraryApp/Models/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryApp.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Введите название книги")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Укажите автора")]
+        public string Author { get; set; }
+
+        public string? CoverImagePath { get; set; } 
+
+        public int? PublishYear { get; set; }
+
+        public string? Description { get; set; }
+        
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        
+        public bool IsAvailable { get; set; } = true;
+    }
+}
